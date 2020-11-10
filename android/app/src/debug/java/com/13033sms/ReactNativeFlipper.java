bae891ac8f67ca1f3eabd3d5fe58b9d9dbd38226
Manosgou <manolisgouvrikos@gmail.com>
2020-11-10 02:10:50 +0200

    Fix package name.

--- android/app/src/debug/java/com/13033sms/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/13033sms/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.13033sms;
+package com.manosgou.smscovid;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
